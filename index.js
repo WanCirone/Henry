d3550af16ee9810feecb99c9c1e230d42e3bbafa
WanCirone <wandacirone@gmail.com>
2021-05-16 18:34:15 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,51 @@
+
+// Crea una variable "string", puede contener lo que quieras:
+const nuevaString = null;
+
+// Crea una variable numérica, puede ser cualquier número:
+const nuevoNum = null;
+
+// Crea una variable booleana:
+const nuevoBool = null;
+
+// Resuelve el siguiente problema matemático:
+const nuevaResta = 10 - null === 5;
+
+// Resuelve el siguiente problema matemático:
+const nuevaMultiplicacion = 10 * null === 40 ;
+
+// Resuelve el siguiente problema matemático:
+const nuevoModulo = 21 % 5 === null;
+
+
+// En los próximos 22 problemas, deberás completar la función.
+// Todo tu código irá dentro de las llaves de la función.
+// Asegúrate que usas "return" cuando la consola te lo pida.
+// Pista: "console.log()" NO fucionará.
+// No cambies los nombres de las funciones.
+
+function devolverString(str) {
+    // "Return" la string provista: str
+    // Tu código:
+  }
+  
+  function suma(x, y) {
+    // "x" e "y" son números
+    // Suma "x" e "y" juntos y devuelve el valor
+    // Tu código:
+  }
+  
+  function resta(x, y) {
+    // Resta "y" de "x" y devuelve el valor
+    // Tu código:
+  }
+  
+  function multiplica(x, y) {
+    // Multiplica "x" por "y" y devuelve el valor
+    // Tu código:
+  }
+  
+  function divide(x, y) {
+    // Divide "x" entre "y" y devuelve el valor
+    // Tu código:
+  }
